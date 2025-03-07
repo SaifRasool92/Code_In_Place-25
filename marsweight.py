634bd5e216f5e60ffdbb87dbfce7b3606353a3bb
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-07 12:31:02 +0500

    Update marsweight.py

--- marsweight.py
+++ marsweight.py
@@ -2,15 +2,19 @@
 MARS_MULTIPLE = 0.378
 
 def main():
+    Earth_weight_str=input("What is your weight on Earth? ")
     #declaring Variable and getting input from user
 
 
 
+
     #typecasting the input in string to float
+    Earth_weight=float(Earth_weight_str)
 
 
 
     #Defining Mars weight calculaton formula and assigning variable
+    Mars_weight=(Earth_weight*Mars)
 
 
 
