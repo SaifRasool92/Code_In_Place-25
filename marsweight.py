6a1610179b79e0936ccc8d18689979ee1efb8535
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-04-12 12:44:04 +0500

    Update marsweight.py

--- marsweight.py
+++ marsweight.py
@@ -1,21 +1,15 @@
 # defining constant as mars multiple
-MARS_MULTIPLE = 0.378
 
 def main():
 
-    #declaring Variable and getting input from user
-    earth_mass=input("what is your weight on earth? ")
+    # getting input from user and storing in variable
 
+    #changing type of the string(taking as input) to float is called typecasting
 
-    #the input in string to float is called typecasting
-    earth_mass_float=float(earth_mass)
-
-     
-    #Defining Mars weight calculation formula and assigning variable
-    mars_mass=(MARS_MULTIPLE*earth_mass_float)
+    #Defining Mars weight calculation formula and storing value in variable mars_weight
 
     #printing string concatenation (combining two strings into one)
-    print("this is equivalent weight on Mars" + str(mars_mass))
+
 if __name__=="__main__":
     main()
 
