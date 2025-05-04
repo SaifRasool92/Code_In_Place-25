b091e7d5d20dc0efdee775a15a3063d68b000a51
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-05-04 22:33:05 +0500

    Update Checkerboard_Karel.py

--- Checkerboard_Karel.py
+++ Checkerboard_Karel.py
@@ -1,21 +1,25 @@
 from karel.stanfordkarel import *
-
 """
-Karel should fill the whole world with beepers.
+
+
+    1. Karel starts at the bottom-left corner, facing East.
+    2. We fill every other square in a checkerboard pattern—column by row.
+    3. After filling a row, Karel moves to the next row(if it exists).
+    4. This process repeats until all accessible rows are filled.
+    5. At the end, Karel returns to the original starting position.
+    
 """
 def main():
     put_beeper()
     complete_east_line()
-    return_to_start()
-
+    return_main()#
 def complete_east_line():
-    alternate_to_wall()
+    alternate_to_wall()#
     turn_left()
     if front_is_clear():
         start_next_line()
         turn_left()
         complete_west_line()
-
 def complete_west_line():
     alternate_to_wall()
     turn_right()
@@ -23,14 +27,12 @@ def complete_west_line():
         start_next_line()
         turn_right()
         complete_east_line()
-
 def start_next_line():
     if beepers_present():
         move()
     else:
         move()
         put_beeper()
-
 def alternate_to_wall():
     while front_is_clear():
         if beepers_present():
@@ -38,31 +40,23 @@ def alternate_to_wall():
         else:
             move()
             put_beeper()
-
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
-
 def turn_around():
     turn_left()
     turn_left()
-
-def return_to_start():
-    # Face South: Karel turns around (because it ends at the top)
+def return_main():
     turn_around()
-    # Move straight down
     while front_is_clear():
         move()
-    # Turn to face West (left)
     turn_right()
     # Move to far-left wall
     while front_is_clear():
         move()
-    # Face East again
     turn_left()
     turn_left()
-
     
 
 
