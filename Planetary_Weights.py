e21b460235c7d542d2763f1d0d5404884a8fb46d
codewithEshaYoutube <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-05-09 17:24:05 +0500

    Update Planetary_Weights.py

--- Planetary_Weights.py
+++ Planetary_Weights.py
@@ -20,35 +20,27 @@ NEPTUNE_GRAVITY = 1.14
 EARTH_GRAVITY = 1.0
 
 def main():
-    # Prompt the user for their weight on Earth
-    earth_weight = float(input("Enter a weight on Earth: "))
-
-    # Prompt the user for the name of a planet
-    planet = input("Enter a planet: ")
-
-    # Determine the gravitational constant for the selected planet
-    if planet == "Mercury":
-        gravity_constant = MERCURY_GRAVITY
-    elif planet == "Venus":
-        gravity_constant = VENUS_GRAVITY
-    elif planet == "Mars":
-        gravity_constant = MARS_GRAVITY
-    elif planet == "Jupiter":
-        gravity_constant = JUPITER_GRAVITY
-    elif planet == "Saturn":
-        gravity_constant = SATURN_GRAVITY
-    elif planet == "Uranus":
-        gravity_constant = URANUS_GRAVITY
+    earth_weight=float(input("Enter your weight on Earth? "))
+    planet=("Enter your Planet? ")
+    if planet==MERCURY:
+        gravity_constant=MERCURY_GRAVITY
+    elif planet==VENUS:
+        gravity_constant=VENUS_GRAVITY_GRAVITY
+    elif planet==MARS:
+        gravity_constant=MARS_GRAVITY_GRAVITY
+    elif planet==SATURN:
+        gravity_constant=SATURN_GRAVITY
+    elif planet==URANUS:
+        gravity_constant=URANUS_GRAVITY
+    elif planet==NEPTUNE:
+        gravity_constant=NEPTUNE_GRAVITY
     else:
-        # can assume user types in one of these planets, so this can be an else instead of elif
-        gravity_constant = NEPTUNE_GRAVITY
+        gravity_constant=EARTH_GRAVITY
 
-    # Calculate the equivalent weight on the selected planet
-    planetary_weight = earth_weight * gravity_constant
-    rounded_planetary_weight = round(planetary_weight, 2)
 
-    # Print the result
-    print("The equivalent weight on " + planet + ":  " + str(rounded_planetary_weight))
+Planet_weight=(gravity_constant * earth_weight)
+rounded_planetary_weight = round(planetary_weight, 2)
+print(f"The weight on {planet} is : {rounded_planetary_weight}")
 
 if __name__ == "__main__":
     main()
